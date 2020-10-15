dd647891cea31c3e2886f6c357175a7d170b94cf
Aurelie Lerouxel <malkiniou@gmail.com>
2020-10-15 15:13:43 +0200

    use the object constructor

--- city.php
+++ city.php
@@ -1,24 +1,31 @@
 <?php 
     class City {
-        protected string $ville;
+        protected ?string $city;
         protected int $departement;
 
-        public function setVille(string $ville) {
-            $this->ville = $ville;
+        public function setCity(?string $city) {
+            $this->city = $city;
+            // return $this;
         }
         public function setDepartement(int $departement) {
             $this->departement = $departement;
+            // return $this;
         }
 
-        public function getVille():string {
-            return $this->ville;
+        public function getCity():string {
+            return $this->city;
         }
         public function getDepartement():int {
             return $this->departement;
         }
 
-        public function showLocation():string {
-            return "La ville " . $this->$ville . " est dans le département " . $this->$departement . " . ";
+        public function showLocation() {
+            return "La ville de " . $this->city . " est dans le département " . $this->departement . " . ";
+        }
+        
+        public function __construct(string $city, int $departement) {
+            $this->setCity($city);
+            $this->setDepartement($departement);
         }
     }
 ?>
